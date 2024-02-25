70fd41c4c729037472c49bddd2c5effa1075d987
rangguy <ranggadwi100@gmail.com>
2024-02-25 15:09:08 +0700

    constraint playlist_id on table activities

--- migrations/1708661728687_create-table-playlist-song-activites.js
+++ migrations/1708661728687_create-table-playlist-song-activites.js
@@ -20,6 +20,12 @@ exports.up = (pgm) => {
       type: "TEXT",
     },
   });
+
+  pgm.addConstraint(
+    "playlist_song_activities",
+    "fk_playlist_song_activities.playlist_id_playlists.id",
+    "FOREIGN KEY (playlist_id) REFERENCES playlists(id) ON DELETE CASCADE",
+  );
 };
 
 exports.down = (pgm) => {
